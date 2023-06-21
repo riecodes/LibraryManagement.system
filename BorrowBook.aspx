f7c397945b60c0ac6e30736b8fe80c6106575ae1
riecodes <eirmonpaculan11@gmail.com>
2023-06-21 17:44:10 +0800

    modified

--- BorrowBook.aspx
+++ BorrowBook.aspx
@@ -4,12 +4,41 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title></title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Borrow Book</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" />
 </head>
 <body>
-    <form id="form1" runat="server">
-        <div>
+    <form id="form2" runat="server">
+        <div class="container mt-5">
+            <h2>Borrow Book</h2>
+            <hr />
+
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="BookIdTextBox">Book ID:</label>
+                        <asp:TextBox ID="BookIdTextBox" runat="server" CssClass="form-control"></asp:TextBox>
+                    </div>
+                    <div class="form-group">
+                        <label for="BookIdDropDownList">Book ID:</label>
+                        <asp:DropDownList ID="BookIdDropDownList" runat="server" CssClass="form-control"></asp:DropDownList>
+                    </div>
+                    <div class="form-group">
+                        <label for="BorrowerIdTextBox">Borrower ID:</label>
+                        <asp:TextBox ID="BorrowerIdTextBox" runat="server" CssClass="form-control"></asp:TextBox>
+                    </div>
+                    <asp:Button ID="BorrowButton" runat="server" Text="Borrow" CssClass="btn btn-primary" OnClick="BorrowButton_Click" />
+                    <asp:Label ID="ErrorMessageLabel" runat="server" Text="" CssClass="text-danger mt-3"></asp:Label>
+                    <asp:Label ID="SuccessMessageLabel" runat="server" Text="" CssClass="text-success mt-3"></asp:Label>
+                </div>
+            </div>
         </div>
     </form>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 </body>
 </html>
--- BorrowBook.aspx.cs
+++ BorrowBook.aspx.cs
@@ -1,10 +1,7 @@
-﻿using MySql.Data.MySqlClient;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Configuration;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
+using System.Data;
+using System.Data.SqlClient;
 using System.Web.UI.WebControls;
 
 namespace LibraryManagement.system
@@ -13,9 +10,108 @@ namespace LibraryManagement.system
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                LoadBookIds();
+            }
         }
-     
 
+        protected void LoadBookIds()
+        {
+            string connectionString = "server=127.0.0.1;port=3306;user=root;password=4412@facebook;database=lib_management_schema;";
+            string query = "SELECT bookid FROM bookinfo WHERE status = 'IN'";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string bookId = reader["bookid"].ToString();
+                            BookIdDropDownList.Items.Add(bookId);
+                        }
+                    }
+                }
+            }
+        }
+
+        protected void BorrowButton_Click(object sender, EventArgs e)
+        {
+            string borrowerId = BorrowerIdTextBox.Text;
+            string bookId = BookIdDropDownList.SelectedValue;
+            DateTime borrowDate = DateTime.Now;
+            DateTime returnDate = borrowDate.AddDays(GetNumberOfDaysAllowed(bookId));
+
+            string connectionString = ConfigurationManager.ConnectionStrings["LibraryManagementSystemConnectionString"].ConnectionString;
+            string query = "INSERT INTO transactioninfo (borrowerid, bookid, borrowdate, returndate) VALUES (@borrowerid, @bookid, @borrowdate, @returndate)";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@borrowerid", borrowerId);
+                    command.Parameters.AddWithValue("@bookid", bookId);
+                    command.Parameters.AddWithValue("@borrowdate", borrowDate);
+                    command.Parameters.AddWithValue("@returndate", returnDate);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            // Update book status to 'OUT'
+            UpdateBookStatus(bookId, "OUT");
+
+            // Clear the form
+            BorrowerIdTextBox.Text = string.Empty;
+            BookIdDropDownList.SelectedIndex = 0;
+
+            SuccessMessageLabel.Text = "Book borrowed successfully!";
+        }
+
+        protected int GetNumberOfDaysAllowed(string bookId)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["LibraryManagementSystemConnectionString"].ConnectionString;
+            string query = "SELECT numberofdaysallowed FROM bookinfo WHERE bookid = @bookid";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@bookid", bookId);
+
+                    connection.Open();
+                    object result = command.ExecuteScalar();
+
+                    if (result != null && result != DBNull.Value)
+                    {
+                        return Convert.ToInt32(result);
+                    }
+
+                    return 0;
+                }
+            }
+        }
+
+        protected void UpdateBookStatus(string bookId, string status)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["LibraryManagementSystemConnectionString"].ConnectionString;
+            string query = "UPDATE bookinfo SET status = @status WHERE bookid = @bookid";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@status", status);
+                    command.Parameters.AddWithValue("@bookid", bookId);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
     }
 }
